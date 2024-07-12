7bd5cb47aa9b4d3b8275b3e537510cc2665ab29a
Laureano Zalazar <lau.zalazar7@gmail.com>
2024-07-12 11:54:59 -0300

    fixing

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -5,7 +5,6 @@ import { AxiosAdapter } from './http/axios.adapter';
 export const movieDBFetcher = new AxiosAdapter({
   baseUrl: 'https://api.themoviedb.org/3/movie',
   params: {
-    // api_key: '20dd0a44fba97e07309e45362931b5b3',
     api_key: THE_MOVIE_DB_KEY ?? 'no-key',
     language: 'es'
   }
